a9664d74818c4c10bb4590e2e73fe962d84aa378
xiaokaichen <13660472014@136.com>
2020-11-12 14:47:04 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+/**
+ * 描述说明:
+ *
+ * @author cxk
+ * @version 1.0
+ */
+@RestController
+public class HelloController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+
+    @RequestMapping(value="/faceImageCheck")
+    public String faceImageCheck() throws IOException {
+
+
+        return "hello world";
+    }
+}
